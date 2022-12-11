902063b7b82a9ae486923e9f3acf72eb37abbcb2
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-12-11 13:02:57 +0100

    style: remove unused files

--- backend/src/live-chat/dto/update-live-chat.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLiveChatDto } from './create-live-chat.dto';
-
-export class UpdateLiveChatDto extends PartialType(CreateLiveChatDto) {
-  id: number;
-}
