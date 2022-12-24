debfaf9b125a5d7c8ee555c64e9b5c9fda43dfc4
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-12-24 23:14:47 +0100

    add validation for username

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,4 +1,8 @@
-export class CreateUserDto  {
+import { MinLength } from "class-validator";
+
+
+export class CreateUserDto {
+  @MinLength(3, {message: `Min length is $constraint1`})
   name?: string;
 
   clientId: string;
