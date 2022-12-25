4b95418274cd139b25dd2fb4503b2e506f793415
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-12-26 00:27:56 +0100

    remove unused socket adapter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
 import { Logger } from "@nestjs/common";
-import { SocketIOAdapter } from "./socket-io-adapter";
 
 
 async function bootstrap() {
@@ -11,7 +10,6 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = parseInt(configService.get("SERVER_PORT"));
 
-  app.useWebSocketAdapter(new SocketIOAdapter(app, configService));
 
   await app.listen(port);
   logger.log(`Server running on port ${port}`);
