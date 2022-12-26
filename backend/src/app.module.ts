7589b640b9ff6f5f68e07125585fe878385b56d6
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-12-26 11:11:24 +0100

    remove unused app CRUD

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,18 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import typeOrmConfig from "./database/config/typeorm.config";
 import { ConfigModule } from "@nestjs/config";
 import { WebScrapperModule } from "./web-scrapper/web-scrapper.module";
-import { MessageModule } from './message/message.module';
-import { UserModule } from './user/user.module';
+import { MessageModule } from "./message/message.module";
+import { UserModule } from "./user/user.module";
 
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRootAsync(typeOrmConfig),
     WebScrapperModule, MessageModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
   exports: [TypeOrmModule]
 })
+
+
 export class AppModule {
 }
