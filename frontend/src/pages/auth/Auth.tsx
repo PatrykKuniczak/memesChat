4a34af002a6d34f8f8937e9b7d8f8b9051555756
masterHAWK99 <maciej.pochron99@gmail.com>
2022-12-26 19:59:00 +0100

    Add support for login

--- frontend/src/pages/auth/Auth.tsx
+++ frontend/src/pages/auth/Auth.tsx
@@ -1,34 +1,47 @@
 import React from "react";
 import {
-    ButtonsContainer,
-    Form,
-    Heading,
-    Input,
-    Label,
-    SignupContainer,
+  ButtonsContainer,
+  Form,
+  Heading,
+  Input,
+  Label,
+  SignupContainer
 } from "./Auth.styled";
-import {NavLink, PrimaryButton} from "../../components/buttons/Button.styled";
-
+import { NavLink, PrimaryButton } from "../../components/buttons/Button.styled";
+import { useParams } from "react-router-dom";
 
 const Auth: React.FC = () => {
-    return (
-        <SignupContainer>
-            <Heading>Rejestracja</Heading>
+  let { type } = useParams();
 
-            <Form action="">
-                <Label htmlFor="login">Login</Label>
-                <Input type="text" id="login" name="login"/>
-                <Label htmlFor="password">Hasło</Label>
-                <Input type="text" id="password" name="password"/>
-                <Label htmlFor="password">Powtórz hasło</Label>
-                <Input type="text" id="password" name="password"/>
-            </Form>
-            <ButtonsContainer>
-                <PrimaryButton>Zarejestruj</PrimaryButton>
-                <NavLink to="signin">Mam juz konto</NavLink>
-            </ButtonsContainer>
-        </SignupContainer>
-    );
+  return (
+    <SignupContainer>
+      <Heading>{type === "signup" ? "Rejestracja" : "Logowanie"}</Heading>
+
+      <Form action="">
+        <Label htmlFor="login">Login</Label>
+        <Input type="text" id="login" name="login" />
+        <Label htmlFor="password">Hasło</Label>
+        <Input type="text" id="password" name="password" />
+        {type === "signup" && (
+          <>
+            <Label htmlFor="password">Powtórz hasło</Label>
+            <Input type="text" id="password" name="password" />
+          </>
+        )}
+      </Form>
+
+      <ButtonsContainer>
+        <PrimaryButton>
+          {type === "signup" ? "Zarejestruj" : "Zaloguj"}
+        </PrimaryButton>
+        {type === "signup" ? (
+          <NavLink to="signin">Mam juz konto</NavLink>
+        ) : (
+          <NavLink to="signup">Nie mam jeszcze konta</NavLink>
+        )}
+      </ButtonsContainer>
+    </SignupContainer>
+  );
 };
 
 export default Auth;
