818a8dd17382d48f262c6c049a3048bb5c97ebca
masterHAWK99 <maciej.pochron99@gmail.com>
2023-01-05 19:03:42 +0100

    Fix code style

--- frontend/src/pages/home/Home.styled.ts
+++ frontend/src/pages/home/Home.styled.ts
@@ -82,15 +82,15 @@ const MainHeader = styled.header`
 `;
 
 const SearchTypeSwitcher = styled.button`
-  background-color: ${(props) => props.theme.gray_500};
-  border: transparent;
-  border-radius: 5px;
-  outline: transparent;
-  padding: 1rem;
-  margin-right: 1rem;
-  color: ${(props) => props.theme.gray_300};
-  font-size: 1rem;
-  text-transform: capitalize;
+    background-color: ${(props) => props.theme.gray_500};
+    border: transparent;
+    border-radius: 5px;
+    outline: transparent;
+    padding: 1rem;
+    margin-right: 1rem;
+    color: ${(props) => props.theme.gray_300};
+    font-size: 1rem;
+    text-transform: capitalize;
 `;
 
 const MessagesWrapper = styled.div`
