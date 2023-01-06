48ec369e913b54ecff24b8719fa6ec972023b695
voinar <38013623+voinar@users.noreply.github.com>
2023-01-06 16:16:20 +0100

    Avatar widget: fix styles

--- frontend/src/components/avatar/Avatar.styled.ts
+++ frontend/src/components/avatar/Avatar.styled.ts
@@ -1,26 +1,31 @@
 import styled from "styled-components";
+import { BsChevronDown } from "react-icons/bs";
 
 const AvatarWrapper = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: right;
-    gap: 6px;
-    padding: 10px;
+    gap: 0.5rem;
+    padding: 0.5rem;
 `;
 
 const Button = styled.button`
     cursor: pointer;
     all: unset;
     background: #ffffff00;
-    padding: 6px;
+    padding: 0.5rem;
     cursor: pointer;
+`;
+
+const ChevronIcon = styled(BsChevronDown)`
+    color: ${(props) => props.theme.gray_300};
 `;
 
 const DropdownWrapper = styled.div`
     position: relative;
-    top: 26px;
-    right: 6px;
+    top: 2rem;
+    right: 0.5rem;
 `;
 
 const DropdownList = styled.ul`
@@ -28,34 +33,31 @@ const DropdownList = styled.ul`
     list-style-type: none;
     white-space: nowrap;
     background-color: ${(props) => props.theme.gray_500};
-    border: transparent;
-    outline: transparent;
     color: ${(props) => props.theme.gray_300};
     right: 0;
-    padding: .5rem;
+    padding: 0.5rem;
     border: 2px solid #00000077;
-    border-radius: .5rem;
+    border-radius: 0.5rem;
 `;
 
 const DropdownListItem = styled.li`
     position: relative;
-    padding: .8rem;
+    padding: 0.8rem;
     cursor: pointer;
 `;
 
 const EditNicknameModal = styled.div`
     display: flex;
-    gap: 10px;
+    gap: 1rem;
     position: absolute;
     top: 30%;
     left: 50%;
     align-items: center;
-    padding: .8rem;
     color: white;
     background: ${(props) => props.theme.gray_500};
     padding: 1rem 2rem;
     border: 2px solid #00000077;
-    border-radius: .5rem;
+    border-radius: 0.5rem;
 `;
 
 const Input = styled.input`
@@ -63,8 +65,8 @@ const Input = styled.input`
     margin: 0.5em;
     color: white;
     background: #3b3b3b;
-    border: none;
-    border-radius: 3px;
+    border: 1px solid grey;
+    border-radius: 0.2rem;
 `;
 
 const DeleteAccountModal = styled.div`
@@ -74,38 +76,51 @@ const DeleteAccountModal = styled.div`
     top: 30%;
     left: 50%;
     align-items: center;
-    padding: .8rem;
     color: white;
     background: ${(props) => props.theme.gray_500};
-    padding: 2rem 2rem;
+    padding: 2rem;
     border: 2px solid #00000077;
-    border-radius: .5rem;
-`
+    border-radius: 0.5rem;
+`;
 
 const DeleteAccountModalButtons = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
-    padding: .8rem;
     color: white;
     padding: 1rem 2rem 0rem;
-    border-radius: .5rem;
-`
+    border-radius: 0.5rem;
+`;
 
 const ButtonPrimary = styled.div`
     background: #6b3c33;
     margin: 0 1rem;
-    padding: .5rem 1rem;
-    border-radius: .2rem;
+    padding: 0.5rem 1rem;
+    border: 1px solid black;
+    border-radius: 0.2rem;
     cursor: pointer;
-`
+`;
 
 const ButtonSecondary = styled.div`
     background: #157350;
     margin: 0 1rem;
-    padding: .5rem 1rem;
-    border-radius: .2rem;
+    padding: 0.5rem 1rem;
+    border: 1px solid black;
+    border-radius: 0.2rem;
     cursor: pointer;
-`
+`;
 
-export { AvatarWrapper, Button, DropdownWrapper, DropdownList, DropdownListItem,EditNicknameModal, Input, DeleteAccountModal, DeleteAccountModalButtons, ButtonPrimary, ButtonSecondary }
+export {
+    AvatarWrapper,
+    Button,
+    DropdownWrapper,
+    DropdownList,
+    DropdownListItem,
+    EditNicknameModal,
+    Input,
+    DeleteAccountModal,
+    DeleteAccountModalButtons,
+    ButtonPrimary,
+    ButtonSecondary,
+    ChevronIcon
+};
