7c51a4d809b85f52994faa51490b330b8805936f
voinar <38013623+voinar@users.noreply.github.com>
2023-01-08 18:13:06 +0100

    feat. state input types.

--- frontend/src/components/avatar/useAvatarFunc.ts
+++ frontend/src/components/avatar/useAvatarFunc.ts
@@ -1,10 +1,16 @@
 import { ChangeEvent, useState } from "react";
 
-export const useAvatarFunc = (userInput = "") => {
-    const [userName, setUserName] = useState<string>("John Doe");
+export const useAvatarFunc = (
+    userInput: ChangeEvent<HTMLInputElement> | string = ""
+) => {
+    const [userName, setUserName] = useState("John Doe");
     const [newUserName, setNewUserName] = useState(userName);
-    const [avatarWidgetStatus, setAvatarWidgetStatus] =
-        useState("dropdown-hidden");
+    const [avatarWidgetStatus, setAvatarWidgetStatus] = useState<
+        | "dropdown-hidden"
+        | "dropdown-visible"
+        | "account-edit-modal-visible"
+        | "account-delete-modal-visible"
+    >("dropdown-hidden");
 
     function handleNicknameChange(userInput: ChangeEvent<HTMLInputElement>) {
         const value = userInput.target.value;
@@ -12,9 +18,9 @@ export const useAvatarFunc = (userInput = "") => {
     }
 
     function toggleAvatarDropdown() {
-        if (avatarWidgetStatus === "dropdown-hidden") {
-            setAvatarWidgetStatus("dropdown-visible");
-        } else setAvatarWidgetStatus("dropdown-hidden");
+        avatarWidgetStatus === "dropdown-hidden"
+            ? setAvatarWidgetStatus("dropdown-visible")
+            : setAvatarWidgetStatus("dropdown-hidden");
     }
 
     function toggleAccountEditModal() {
