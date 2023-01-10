b9e7ff24f1307301f754769b96fef381f4163b99
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-01-10 18:24:55 +0100

    clean code and create orm config type

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,15 +3,14 @@ import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
 import { Logger } from "@nestjs/common";
 
-
 async function bootstrap() {
-  const logger = new Logger("Main");
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = parseInt(configService.get("SERVER_PORT"));
+	const logger = new Logger("Main");
+	const app = await NestFactory.create(AppModule);
+	const configService = app.get(ConfigService);
+	const port = parseInt(configService.get("SERVER_PORT"));
 
-  await app.listen(port);
-  logger.log(`Server running on port ${port}`);
+	await app.listen(port);
+	logger.log(`Server running on port ${port}`);
 }
 
 bootstrap();
