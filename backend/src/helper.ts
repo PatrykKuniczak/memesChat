1bbde8c97872b807d7dac287584e965fdb1cd7d0
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-01-14 11:54:21 +0100

    remove validation func

--- backend/src/helper.ts
+++ backend/src/helper.ts
@@ -1,17 +1,6 @@
-import { validateOrReject } from "class-validator";
 import * as dotenv from "dotenv";
-
-async function isValid(input) {
-	try {
-		return await validateOrReject(input);
-	} catch (err) {
-		return err;
-	}
-}
 
 dotenv.config({ path: "./.env" });
 
 export const WS_CLIENT_PORT = +process.env.WS_PORT;
 export const WS_CLIENT_URL = process.env.WS_URL;
-
-export default isValid;
