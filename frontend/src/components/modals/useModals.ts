b9158ba984b65338598addcdc619e2d97b39bc4a
Skolaczk <skolakmichal1@gmail.com>
2023-01-24 11:50:50 +0100

    Add a closing menu and modals on esc key

--- frontend/src/components/modals/useModals.ts
+++ frontend/src/components/modals/useModals.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const useModals = (changeMenuVisible: () => void) => {
     const [currentModal, setCurrentModal] = useState("");
@@ -11,6 +11,16 @@ const useModals = (changeMenuVisible: () => void) => {
         setCurrentModal(modalName);
         changeMenuVisible();
     };
+
+    useEffect(() => {
+        const keyDownHandler = (event: KeyboardEvent) => {
+            if (event.key === "Escape") hideModal();
+        };
+
+        if (currentModal) document.addEventListener("keydown", keyDownHandler);
+
+        return () => document.removeEventListener("keydown", keyDownHandler);
+    }, [currentModal]);
 
     return { currentModal, hideModal, showModal };
 };
