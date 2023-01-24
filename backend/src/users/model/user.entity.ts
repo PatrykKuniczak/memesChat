c993eb321f42fabb3efcfb8607be897da7f175b9
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-01-24 21:15:50 +0100

    create authentication

---
+++ backend/src/users/model/user.entity.ts
@@ -0,0 +1,32 @@
+import {
+	BeforeInsert,
+	BeforeUpdate,
+	Column,
+	Entity,
+	PrimaryGeneratedColumn
+} from "typeorm";
+
+@Entity("User")
+export class User {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({ unique: true, length: 50 })
+	username: string;
+
+	@Column({ select: false })
+	password?: string;
+
+	// FOR NEXT FEATURE
+	// @Column()
+	// avatar: string
+
+	// @OneToMany(() => MessageEntity, message => message.user, {nullable: true})
+	// messages: Message[];
+
+	@BeforeInsert()
+	@BeforeUpdate()
+	emailAndUsernameToLowerCase?() {
+		this.username = this.username.toLowerCase();
+	}
+}
