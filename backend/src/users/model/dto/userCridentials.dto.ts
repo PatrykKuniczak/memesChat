91dc38d6004f89ca732fb16fb6ae27fc5ef831eb
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-01-24 21:24:19 +0100

    create max password length

--- backend/src/users/model/dto/userCridentials.dto.ts
+++ backend/src/users/model/dto/userCridentials.dto.ts
@@ -1,9 +1,9 @@
-import { Length, MinLength } from "class-validator";
+import { Length } from "class-validator";
 
 export class UserCridentialsDto {
 	@Length(10, 50)
 	username: string;
 
-	@MinLength(10)
+	@Length(10, 80)
 	password: string;
 }
