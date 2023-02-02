b213f4b57df28c5221ba991fb810473076269ad0
masterHAWK99 <maciej.pochron99@gmail.com>
2023-02-02 16:10:29 +0100

    Remove unnecessary code

--- frontend/src/components/messages/hooks/useMessages.ts
+++ frontend/src/components/messages/hooks/useMessages.ts
@@ -6,36 +6,28 @@ const UseMessages = () => {
     >([]);
 
     useEffect(() => {
-        let ignore = true;
-
         const fetchData = async () => {
             const response = await fetch(`https://dummyjson.com/comments`);
             const json = await response.json();
 
-            if (ignore) {
-                setMessages(
-                    json.comments.map(
-                        (comment: {
-                            id: string;
-                            body: string;
-                            user: { username: string };
-                        }) => {
-                            return {
-                                id: comment.id,
-                                message: comment.body,
-                                author: comment.user.username
-                            };
-                        }
-                    )
-                );
-            }
+            setMessages(
+                json.comments.map(
+                    (comment: {
+                        id: string;
+                        body: string;
+                        user: { username: string };
+                    }) => {
+                        return {
+                            id: comment.id,
+                            message: comment.body,
+                            author: comment.user.username
+                        };
+                    }
+                )
+            );
         };
 
         fetchData().catch(console.error);
-
-        return () => {
-            ignore = false;
-        };
     }, []);
 
     const handleSetMessages = (
