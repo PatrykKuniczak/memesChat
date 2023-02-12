4f7a854a5eb94bdb6f92c962be4048e8de435f1d
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-02-12 15:12:27 +0100

    rename dir

--- frontend/src/components/modals/Modals.tsx
+++ frontend/src/components/modals/Modals.tsx
@@ -1,5 +1,5 @@
 import DropdownMenu from "./dropdown/DropdownMenu";
-import EditAccountModal from "./editName/EditAccountModal";
+import EditAccountModal from "./editAccount/EditAccountModal";
 import DeleteAccountModal from "./accountDelete/DeleteAccountModal";
 import useModals from "./useModals";
 import { IModals } from "./modals.interfaces";
@@ -7,28 +7,30 @@ import { ModalsWrapper } from "./Modals.styled";
 import DeleteAvatarModal from "./removeAvatar/DeleteAvatarModal";
 
 const Modals = ({ showMenu, changeMenuVisible }: IModals) => {
-	const { currentModal, hideModal, showModal } = useModals(changeMenuVisible);
+    const { currentModal, hideModal, showModal } = useModals(changeMenuVisible);
 
-	return (
-		<ModalsWrapper>
-			{showMenu && (
-				<DropdownMenu
-					showModal={showModal}
-					changeMenuVisible={changeMenuVisible}
-				/>
-			)}
+    return (
+        <ModalsWrapper>
+            {showMenu && (
+                <DropdownMenu
+                    showModal={showModal}
+                    changeMenuVisible={changeMenuVisible}
+                />
+            )}
 
-			{currentModal === "edit" && <EditAccountModal hideModal={hideModal} />}
+            {currentModal === "edit" && (
+                <EditAccountModal hideModal={hideModal} />
+            )}
 
-			{currentModal === "deleteAvatar" && (
-				<DeleteAvatarModal hideModal={hideModal} />
-			)}
+            {currentModal === "deleteAvatar" && (
+                <DeleteAvatarModal hideModal={hideModal} />
+            )}
 
-			{currentModal === "deleteAccount" && (
-				<DeleteAccountModal hideModal={hideModal} />
-			)}
-		</ModalsWrapper>
-	);
+            {currentModal === "deleteAccount" && (
+                <DeleteAccountModal hideModal={hideModal} />
+            )}
+        </ModalsWrapper>
+    );
 };
 
 export default Modals;
