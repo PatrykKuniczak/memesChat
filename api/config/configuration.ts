31f51f60919015b09b3e1fa16ed0f630c2d6875a
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-03-12 12:58:55 +0100

    fix: wrong value in env for postgres

--- api/config/configuration.ts
+++ api/config/configuration.ts
@@ -7,9 +7,9 @@ export const configuration = () => ({
         type: process.env.DB_TYPE,
         host: process.env.POSTGRES_HOST,
         port: +process.env.POSTGRES_PORT,
-        username: process.env.POSTGRES_USERNAME,
+        username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DATABASE,
+        database: process.env.POSTGRES_DB,
     },
     devOptions: {
         development: process.env.DEVELOPMENT,
