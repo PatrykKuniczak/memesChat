52a02b84bf7ff88d88377e51fced8f9db902b975
masterHAWK99 <maciej.pochron99@gmail.com>
2023-04-10 20:14:16 +0200

    Add react-query and setting token on entry

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
 import { THEME_DARK } from "assets/styles/theme";
+import axios from "axios";
 import GlobalStyles from "./assets/GlobalStyles.styled";
 import Auth from "./pages/auth/Auth";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import ProtectedPage from "./ProtectedRoute";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { updateInterceptor } from "./helpers/axios/AuthIncereptor";
+import useToken from "./hooks/useToken";
+
+export const API_URL = process.env.REACT_APP_API_URL;
+axios.defaults.baseURL = API_URL;
+
+const queryClient = new QueryClient();
 
 const App = () => {
+    const { userToken } = useToken();
+
+    updateInterceptor(userToken);
+
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <GlobalStyles />
 
             <ThemeProvider theme={THEME_DARK}>
@@ -35,7 +48,7 @@ const App = () => {
                     </Routes>
                 </BrowserRouter>
             </ThemeProvider>
-        </>
+        </QueryClientProvider>
     );
 };
 
---
+++ frontend/src/helpers/axios/AuthIncereptor.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const updateInterceptor = (token: string) => {
+    axios.interceptors.request.use(
+        config => {
+            config.headers["Authorization"] = `Bearer ${token}`;
+            return config;
+        },
+        error => {
+            return Promise.reject(error);
+        }
+    );
+};
