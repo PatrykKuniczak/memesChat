00b9f0a90c858a8fd996d4069731a1d09dc58d40
masterHAWK99 <maciej.pochron99@gmail.com>
2023-04-11 15:41:37 +0200

    Implement functionality for deleting avatar

--- frontend/src/components/modals/removeAvatar/DeleteAvatarModal.tsx
+++ frontend/src/components/modals/removeAvatar/DeleteAvatarModal.tsx
@@ -4,26 +4,34 @@ import useRemoveAvatar from "./useDeleteAvatar";
 import { ModalSpan } from "../GenericModalComponents.styled";
 import { PrimaryButton, SecondaryButton } from "../../buttons/Button.styled";
 import {
-	DeleteAvatarButtonsWrapper,
-	DeleteAvatarWrapper
+    DeleteAvatarButtonsWrapper,
+    DeleteAvatarWrapper
 } from "./DeleteAvatarModal.styled";
+import { ErrorMessage } from "assets/styles/theme";
 
 const DeleteAvatarModal = ({ hideModal }: IModal) => {
-	const { deleteAvatarHandler } = useRemoveAvatar(hideModal);
+    const { deleteAvatarHandler, isError, error } = useRemoveAvatar(hideModal);
 
 	const { ref } = useClickOutside(hideModal);
 
-	return (
-		<DeleteAvatarWrapper ref={ref}>
-			<ModalSpan>Czy na pewno chcesz usunąć avatar?</ModalSpan>
-			<DeleteAvatarButtonsWrapper>
-				<SecondaryButton type="submit" onClick={deleteAvatarHandler}>
-					Tak, usuwam avatar
-				</SecondaryButton>
-				<PrimaryButton type="button" onClick={hideModal}>Anuluj</PrimaryButton>
-			</DeleteAvatarButtonsWrapper>
-		</DeleteAvatarWrapper>
-	);
+    return (
+        <DeleteAvatarWrapper
+            ref={ref}
+            onSubmit={deleteAvatarHandler}>
+            <ModalSpan>Czy na pewno chcesz usunąć avatar?</ModalSpan>
+            <DeleteAvatarButtonsWrapper>
+                <SecondaryButton type="submit">
+                    Tak, usuwam avatar
+                </SecondaryButton>
+                <PrimaryButton
+                    type="button"
+                    onClick={hideModal}>
+                    Anuluj
+                </PrimaryButton>
+            </DeleteAvatarButtonsWrapper>
+            <>{isError && <ErrorMessage>{error?.message}</ErrorMessage>}</>
+        </DeleteAvatarWrapper>
+    );
 };
 
 export default DeleteAvatarModal;
