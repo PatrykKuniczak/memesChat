e41e419ab340c854ebfa649f3544d10d6eadb16c
masterHAWK99 <maciej.pochron99@gmail.com>
2023-04-13 16:15:34 +0200

    Move users mapping to logic component

--- frontend/src/components/users/Users.tsx
+++ frontend/src/components/users/Users.tsx
@@ -1,37 +1,12 @@
 import { UsersContainer } from "./Users.styled";
-import useUsers from "./useUsers";
-import User, { IUser } from "../user/User";
+import { IUser } from "../user/User";
 import Search from "components/search/Search";
+import useUsersContainer from "./UsersContainer";
 
 export type IUsers = IUser[];
 
 const Users = () => {
-    const { handleChange, filteredUsers, isLoading, error } = useUsers();
-
-    const UsersList = () => {
-        return (
-            <>
-                {isLoading ? (
-                    <p style={{ color: "whitesmoke", paddingTop: "1rem" }}>
-                        Ładowanie...
-                    </p>
-                ) : error ? (
-                    <p style={{ color: "indianred", paddingTop: "1rem" }}>
-                        Wystąpił błąd podczas ładowania danych.
-                    </p>
-                ) : (
-                    filteredUsers.map(({ id, username, userAvatar }: IUser) => (
-                        <User
-                            key={id}
-                            id={id}
-                            username={username}
-                            userAvatar={userAvatar}
-                        />
-                    ))
-                )}
-            </>
-        );
-    };
+    const { UsersList, handleChange } = useUsersContainer();
 
     return (
         <UsersContainer>
